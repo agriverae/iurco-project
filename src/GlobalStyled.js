93178a8f119907a3a0f8287d87f7aa79e6b3c38f
Ali Rivera <user@MAC-0280.local>
2021-05-04 12:18:06 -0300

    updated fonts

--- src/GlobalStyled.js
+++ src/GlobalStyled.js
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyled = createGlobalStyle`
+  html {
+    font-family:'Montserrat','Oswald', 'Poppins',sans-serif;
+  }
+
   body {
     margin: 0;
     padding: 0;
