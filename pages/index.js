0769e253ae531ffcf55ee5f53308d6bca2d621ba
agriverae <31800323+agriverae@users.noreply.github.com>
2021-05-10 13:54:46 -0300

    finished trabajemos (#3)
    * finished trabajemos
    
    * trabajemos
    
    Co-authored-by: wen <wendy.wettel@25watts.com.ar>

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Metodologias from "@/components/Metodologias";
-
+import Trabajemos from "@/components/Trabajemos";
 function Home() {
   return (
     <>
       <Header />
       <Metodologias />
+      <Trabajemos />
       <Footer />
     </>
   );
---
+++ src/components/Trabajemos/Trabajemos.jsx
@@ -0,0 +1,16 @@
+import TrabajemosStyled from "./TrabajemosStyled";
+
+function Trabajemos() {
+  return (
+    <TrabajemosStyled>
+      <div>
+        <h2>
+          TRABAJEMOS JUNTOS <span>EN ALCANZAR TUS METAS</span>
+        </h2>
+        <button>Empezá tu proyecto</button>
+      </div>
+    </TrabajemosStyled>
+  );
+}
+
+export default Trabajemos;
